9308a3dd238b354e9a5c90094371aec837f026b0
BlckSoftware <huseyinkarayazim@gmail.com>
2023-05-29 11:20:29 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace HK.Data_Encrypt_Decrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.encrypt_textbox = new System.Windows.Forms.TextBox();
+            this.decrypt_textbox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(52, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Encrypt :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(52, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Decrypt :";
+            // 
+            // encrypt_textbox
+            // 
+            this.encrypt_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.encrypt_textbox.Location = new System.Drawing.Point(57, 49);
+            this.encrypt_textbox.Name = "encrypt_textbox";
+            this.encrypt_textbox.Size = new System.Drawing.Size(476, 31);
+            this.encrypt_textbox.TabIndex = 2;
+            this.encrypt_textbox.TextChanged += new System.EventHandler(this.encrypt_textbox_TextChanged);
+            // 
+            // decrypt_textbox
+            // 
+            this.decrypt_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.decrypt_textbox.Location = new System.Drawing.Point(55, 128);
+            this.decrypt_textbox.Name = "decrypt_textbox";
+            this.decrypt_textbox.Size = new System.Drawing.Size(476, 31);
+            this.decrypt_textbox.TabIndex = 3;
+            this.decrypt_textbox.TextChanged += new System.EventHandler(this.decrypt_textbox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(637, 228);
+            this.Controls.Add(this.decrypt_textbox);
+            this.Controls.Add(this.encrypt_textbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "HK Encrypt & Decrypt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox encrypt_textbox;
+        private System.Windows.Forms.TextBox decrypt_textbox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HK.Data_Encrypt_Decrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void decrypt_textbox_TextChanged(object sender, EventArgs e)
+        {
+            encrypt_textbox.Text = HK.Security.StringCipher.Decrypt(decrypt_textbox.Text);
+
+        }
+        private void encrypt_textbox_TextChanged(object sender, EventArgs e)
+        {
+            decrypt_textbox.Text = HK.Security.StringCipher.Encrypt(encrypt_textbox.Text);
+        }
+    }
+}
